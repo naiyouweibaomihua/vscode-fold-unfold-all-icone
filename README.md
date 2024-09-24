0f88b446ff15463264afc926dfb833fed2af212e
blackbeardd <blackbeardd@163.com>
2024-09-24 09:22:24 +0800

    1

---
+++ README.md
@@ -0,0 +1,7 @@
+# fold-unfold-all-icone README
+Just add a fold and a unfold button in the status bar (at the bottom left).
+
+![preview fold / unfold icone](preview.gif)
+
+[![button](GitHub-Mark-64px.png)](https://github.com/Benjioe/vscode-fold-unfold-all-icone)
+[vscode marketplace](https://marketplace.visualstudio.com/items?itemName=FerrierBenjamin.fold-unfold-all-icone)
---
+++ src/extension.ts
@@ -0,0 +1,25 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate({subscriptions}: vscode.ExtensionContext) {
+	const foldStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+	foldStatusBarItem.command = "editor.foldLevel1";
+	foldStatusBarItem.tooltip = "fold Level 1";
+	foldStatusBarItem.text = `{$(chevron-right)$(chevron-left)}`;
+	subscriptions.push(foldStatusBarItem);
+	foldStatusBarItem.show();
+
+
+	const unfoldStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+	unfoldStatusBarItem.command = "editor.unfoldAll";
+	unfoldStatusBarItem.tooltip = "unfold all";
+	unfoldStatusBarItem.text = `{$(chevron-left)$(chevron-right)}`;
+	subscriptions.push(unfoldStatusBarItem);
+	unfoldStatusBarItem.show();
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
